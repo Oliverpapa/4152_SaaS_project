ccf93943e65846183377b45d3ca125c11bd326b8
Zifan Chen <zc2628@columbia.edu>
2022-10-29 16:59:16 -0400

    Delete rottenpotatoes directory

--- rottenpotatoes/app/models/attraction.rb
+++
@@ -1,3 +0,0 @@
-class Attraction < ActiveRecord::Base
-  
-end
--- rottenpotatoes/config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Trapplar::Application.config.session_store :cookie_store, key: '_Trapplar_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Trapplar::Application.config.session_store :active_record_store
